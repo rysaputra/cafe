b471a5b47a272f7120eabc802ec523f61ca4605d
Ryo <158389572+rysaputra@users.noreply.github.com>
2024-07-25 15:15:41 +0700

    updtae

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,15 +26,8 @@ const config: Config = {
       },
     },
   },
-<<<<<<< HEAD
+
   plugins: [],
-=======
-  plugins: [
-    function ({ }) {
-        ['responsive']
-      );
-    },
-  ],
->>>>>>> 78946e53bd7606f36ba1b49570ea7fb580078acd
+
 };
 export default config;
