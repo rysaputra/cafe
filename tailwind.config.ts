40c2b2a75b6e2dbf48b23c2323482cebd56b844d
Ryo <158389572+rysaputra@users.noreply.github.com>
2024-07-25 15:20:37 +0700

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,26 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      scrollBehavior: {
-        smooth: 'smooth',
-      },
-      fontFamily: {
-        'sans': ['Plus Jakarta Sans', 'sans-serif'],
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-    variants: {
-      extend: {
-        scrollBehavior: ['responsive'],
-      },
-    },
   },
 
   plugins: [],
-
+  }
 };
 export default config;
